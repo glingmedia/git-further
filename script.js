43c10b8bb70279b5da16b94323e37e5f22a3f056
glingmedia <jonas.glingert@medieinstitutet.se>
2020-10-23 13:53:11 +0200

    added RNG

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Start function when button is clicked
 function printRandomNumber() {
-    console.log('test');
+    const randomNumber = getRandomNumber();
+    addNumberToPage(randomNumber)
+}
+
+// Helper function 1
+function getRandomNumber() {
+    const randomNumber = Math.random() * 100
+    return Math.round(randomNumber);
+}
+
+// Helper function 2
+function addNumberToPage(number) {
+    console.log(number);
 }
